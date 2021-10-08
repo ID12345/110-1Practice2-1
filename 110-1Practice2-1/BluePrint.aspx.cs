9ed142c3008204d632a956d0e5d68b3b20efc38a
達恩 范 <108111124@mail.aeust.edu.tw>
2021-10-08 10:44:13 +0800

    BluePrint

--- 110-1Practice2-1/BluePrint.aspx.cs
+++ 110-1Practice2-1/BluePrint.aspx.cs
@@ -12,20 +12,21 @@ namespace _110_1Practice2_1
         public class Man {
             public string s_Id;
             public float f_Hei;
-            public float f Wei;
+            public float f_Wei;
         }
 
         protected void Page_Load(object sender, EventArgs e)
         {
-            Man o Man = new Man();
-            o Man.s_Id ="A123456789";
-            o Man.f_Hei = 178.5f;
+            Man o_Man = new Man();
+            o_Man.s_Id = "A123456789";
+            o_Man.f_Hei = 178.5f;
             o_Man.f_Wei = 72.8f;
 
-            // s_Id = 值，千_Hei =•值，千_ wei =•值
-            Response.Write("s_Id=" + 0_Man.S_Id + ", " +
-            "f_Hei=" O_Man.F_Hei.ToString() + ", " +
-            "F_Wei =" + 0_Man.f_Wei.ToString();
+            // s_Id = 值， f_Hei = 值， f_wei = 值
+            Response.Write("s_Id=" + o_Man.s_Id + ", " +
+            "f_Hei=" + o_Man.f_Hei.ToString() + ", " +
+            "f_Wei =" + o_Man.f_Wei.ToString()
+            );
         }
     }
 }
